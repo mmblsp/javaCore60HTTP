0359612dbc7157e0e50860aa6a38a118ed83f388
Anatoly Zhuchkov <a.zhuchkov@arenadata.io>
2024-05-21 17:28:44 +0300

    finalized

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -5,14 +5,14 @@ public class Cat {
     private final String text;
     private final String type;
     private final String user;
-    private final String upvotes;
+    private final int upvotes;
 
     public Cat(
             @JsonProperty("id") String id,
             @JsonProperty("text") String text,
             @JsonProperty("type") String type,
             @JsonProperty("user") String user,
-            @JsonProperty("upvotes") String upvotes)
+            @JsonProperty("upvotes") int upvotes)
     {
         this.id = id;
         this.text = text;
@@ -37,7 +37,7 @@ public class Cat {
         return user;
     }
 
-    public String getUpvotes() {
+    public int getUpvotes() {
         return upvotes;
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static final String REMOTE_SERVICE_URI =
@@ -15,18 +15,21 @@ public class Main {
     public static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
+        try(
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
                         .setSocketTimeout(30000)
                         .setRedirectsEnabled(false)
                         .build())
-                .build();
-        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-        CloseableHttpResponse response = httpClient.execute(request);
-        List<Cat> cats = mapper.readValue(response.getEntity().getContent(),
-                new TypeReference<>() {});
-        cats.stream().filter(value -> value.getUpvotes() != null && Integer.parseInt(value.getUpvotes()) > 0)
-                .forEach(System.out::println);
+                .build()){
+                        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+                        try (CloseableHttpResponse response = httpClient.execute(request)){
+                                List<Cat> cats = mapper.readValue(response.getEntity().getContent(),
+                                new TypeReference<>() {});
+                                cats.stream().filter(value -> value.getUpvotes() > 0)
+                                .forEach(System.out::println);
+                        }
+                }
     }
 }
